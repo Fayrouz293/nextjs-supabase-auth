4ae7d8657c62dbfaf35dea93a6df072b9519367a
Fairuz Al Adli Putra <142369245+Fayrouz293@users.noreply.github.com>
2025-11-08 11:25:40 +0700

    Update page.js

--- src/app/sign-in/page.js
+++ src/app/sign-in/page.js
@@ -6,11 +6,15 @@ import SignIn from 'src/components/Auth/SignIn';
 
 export default async function SignInPage() {
   const supabase = createServerComponentClient({ cookies });
+
+  // Ambil session user
   const { data } = await supabase.auth.getSession();
 
+  // Kalau sudah login → langsung ke halaman barang
   if (data?.session) {
     redirect('/barang');
   }
 
+  // Kalau belum login → tampilkan form login
   return <SignIn />;
 }
